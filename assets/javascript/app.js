d488e2829664e9a188beeed88b10efa79dc85c32
Jeff Swearingen <jeffswearingen@gmail.com>
2016-11-02 15:34:53 -0400

    timer issues

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -200,6 +200,7 @@ function setup() {
 	$('.question').append('<button id="startButton">Start</button>');
 	$('#startButton').on('click', function() {
 		$(this).hide();
+		countdownTimer.start();
 	 	loadQuestion(index);
 	});
 }		
